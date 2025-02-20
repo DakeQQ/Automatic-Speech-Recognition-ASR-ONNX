fe2427e712c740c72cffd3defb875dda967fe23e
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-02-20 17:26:14 +0800

    Update Export_SenseVoice.py

--- SenseVoice/Export_SenseVoice.py
+++ SenseVoice/Export_SenseVoice.py
@@ -71,10 +71,8 @@ class SENSE_VOICE(torch.nn.Module):
         mel_features = torch.cat((self.language_embed[:, language_idx].float(), self.system_embed, mel_features), dim=1)
         encoder_out = self.encoder((mel_features + self.cmvn_means) * self.cmvn_vars)
         token_ids = self.ctc_lo(encoder_out).argmax(dim=-1).int()
-        shifted_tensor = torch.roll(token_ids, shifts=-1, dims=-1)
-        mask = ((token_ids != shifted_tensor) & (token_ids != self.blank_id)).to(torch.int32)
-        mask[..., 0] = 1
-        return token_ids.index_select(-1, torch.nonzero(mask, as_tuple=True)[-1])
+        token_ids = torch.unique(token_ids, sorted=False)
+        return token_ids.to(torch.int32)[token_ids != self.blank_id]
 
 
 print('\nExport start ...\n')
@@ -107,7 +105,7 @@ with torch.inference_mode():
         do_constant_folding=True,
         dynamic_axes={
             'audio': {2: 'audio_len'},
-            'token_ids': {1: 'num_token'}
+            'token_ids': {0: 'num_token'}
         } if DYNAMIC_AXES else None,
         opset_version=17
     )
@@ -184,7 +182,7 @@ for language_idx, test in enumerate(test_audio):
                 in_name_A1: language_idx
             })[0]
         end_time = time.time()
-        text = tokenizer.decode(token_ids.tolist())[0]
+        text = tokenizer.decode(token_ids.tolist())
         slice_start += stride_step
         slice_end = slice_start + INPUT_AUDIO_LENGTH
         print(f"\nASR Result:\n{text}\n\nTime Cost: {end_time - start_time:.3f} Seconds\n")
