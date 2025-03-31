5ae0c963bbe9ccf40f416d49d58633a7cb89fd10
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-03-31 16:07:57 +0800

    Update Inference_Whisper_ONNX.py

--- Whisper_V3/Inference_Whisper_ONNX.py
+++ Whisper_V3/Inference_Whisper_ONNX.py
@@ -174,7 +174,7 @@ for language_idx, test in enumerate(test_audio):
         language = test.split("/")[-1].split(".")[0]
     else:
         language = TARGET_LANGUAGE
-    audio = np.array(AudioSegment.from_file(test).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples())
+    audio = np.array(AudioSegment.from_file(test).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.int16)
     audio_len = len(audio)
     audio = audio.reshape(1, 1, -1)
     if isinstance(shape_value_in, str):
