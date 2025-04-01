7e9fc6606f5c4f80f744c3380310791c18fd8868
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-04-01 11:08:07 +0800

    Update Inference_Whisper_ONNX.py

--- Whisper_V3/Inference_Whisper_ONNX.py
+++ Whisper_V3/Inference_Whisper_ONNX.py
@@ -23,7 +23,7 @@ STOP_TOKEN = 50257                      # 50257 is the end token for common Whis
 
 
 def normalize_to_int16(audio):
-    max_val = np.max(np.abs(audio.astype(np.float32)))
+    max_val = np.max(np.abs(audio))
     scaling_factor = 32767.0 / max_val if max_val > 0 else 1.0
     return (audio * float(scaling_factor)).astype(np.int16)
   
@@ -180,7 +180,7 @@ for language_idx, test in enumerate(test_audio):
         language = test.split("/")[-1].split(".")[0]
     else:
         language = TARGET_LANGUAGE
-    audio = np.array(AudioSegment.from_file(test).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.int32)
+    audio = np.array(AudioSegment.from_file(test).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.float32)
     audio = normalize_to_int16(audio)
     audio_len = len(audio)
     audio = audio.reshape(1, 1, -1)
