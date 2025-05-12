a00da56b35dc6b68f6637dada76c719264eaab71
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-05-12 11:38:29 +0800

    Update Optimize_ONNX_q8f32.py

--- Whisper_V2_V3/Optimize_ONNX_q8f32.py
+++ Whisper_V2_V3/Optimize_ONNX_q8f32.py
@@ -21,28 +21,31 @@ quanted_model_path = os.path.join(quanted_folder_path, "Whisper_Encoder.onnx")  
 # model_path = os.path.join(original_folder_path, "Whisper_Decoder.onnx")          # The original fp32 model path.
 # quanted_model_path = os.path.join(quanted_folder_path, "Whisper_Decoder.onnx")   # The optimized model stored path.
 download_path = r'/home/DakeQQ/Downloads/whisper-large-v3'                       # Set the folder path where the LLM whole project downloaded, otherwise set "NONE".
+quant_int8 = True                                                                # Quant the model to int8 format.
+quant_float16 = False                                                            # Quant the model to float16 format.
 use_gpu = False                                                                  # If true, the transformers.optimizer will remain the FP16 processes.
 provider = 'CPUExecutionProvider'                                                # ['CPUExecutionProvider', 'CUDAExecutionProvider']
 use_low_memory_mode_in_Android = False                                           # If you need to use low memory mode on Android, please set it to True.
-upgrade_opset = 21                                                               # Optional process. Set 0 for close.
+upgrade_opset = 17                                                               # Optional process. Set 0 for close.
 
 
 # Start Quantize
-quantize_dynamic(
-    model_input=quant_utils.load_model_with_shape_infer(Path(model_path)),
-    model_output=quanted_model_path,
-    per_channel=True,                                        # True for model accuracy but cost a lot of time during quanting process.
-    reduce_range=False,                                      # True for some x86_64 platform.
-    weight_type=QuantType.QUInt8,                            # It is recommended using uint8 + Symmetric False
-    extra_options={'ActivationSymmetric': False,             # True for inference speed. False may keep more accuracy.
-                   'WeightSymmetric': False,                 # True for inference speed. False may keep more accuracy.
-                   'EnableSubgraph': True,                   # True for more quant.
-                   'ForceQuantizeNoInputCheck': False,       # True for more quant.
-                   'MatMulConstBOnly': True                  # False for more quant. Sometime, the inference speed may get worse.
-                   },
-    nodes_to_exclude=None,                                   # Specify the node names to exclude quant process. Example: nodes_to_exclude={'/Gather'}
-    use_external_data_format=True                            # Save the model into two parts.
-)
+if quant_int8:
+    quantize_dynamic(
+        model_input=quant_utils.load_model_with_shape_infer(Path(model_path)),
+        model_output=quanted_model_path,
+        per_channel=True,                                        # True for model accuracy but cost a lot of time during quanting process.
+        reduce_range=False,                                      # True for some x86_64 platform.
+        weight_type=QuantType.QUInt8,                            # It is recommended using uint8 + Symmetric False
+        extra_options={'ActivationSymmetric': False,             # True for inference speed. False may keep more accuracy.
+                       'WeightSymmetric': False,                 # True for inference speed. False may keep more accuracy.
+                       'EnableSubgraph': True,                   # True for more quant.
+                       'ForceQuantizeNoInputCheck': False,       # True for more quant.
+                       'MatMulConstBOnly': True                  # False for more quant. Sometime, the inference speed may get worse.
+                       },
+        nodes_to_exclude=None,                                   # Specify the node names to exclude quant process. Example: nodes_to_exclude={'/Gather'}
+        use_external_data_format=True                            # Save the model into two parts.
+    )
 
 
 model_size_bytes = sys.getsizeof(onnx.load(quanted_model_path).SerializeToString())
@@ -89,6 +92,14 @@ model = optimize_model(quanted_model_path,
                        provider=provider,
                        verbose=False,
                        model_type='bert')
+if quant_float16:
+    model.convert_float_to_float16(
+        keep_io_types=False,
+        force_fp16_initializers=True,
+        use_symbolic_shape_infer=True,  # True for more optimize but may get errors.
+        max_finite_val=65504.0,
+        op_block_list=['DynamicQuantizeLinear', 'DequantizeLinear', 'DynamicQuantizeMatMul', 'Range', 'MatMulIntegerToFloat']
+    )
 model.save_model_to_file(quanted_model_path, use_external_data_format=is_large_model)
 del model
 gc.collect()
