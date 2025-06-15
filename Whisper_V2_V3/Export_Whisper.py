ea53f8ea57d4fcbc7ea97ca7c14b9b2012fb4480
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-06-15 23:09:47 +0800

    Update Export_Whisper.py

--- Whisper_V2_V3/Export_Whisper.py
+++ Whisper_V2_V3/Export_Whisper.py
@@ -175,7 +175,7 @@ class WHISPER_ENCODER(torch.nn.Module):
 
     def forward(self, audio):
         audio = audio.float() * self.inv_int16
-        audio -= torch.mean(audio)  # Remove DC Offset
+        audio = audio - torch.mean(audio)  # Remove DC Offset
         audio = torch.cat((audio[:, :, :1], audio[:, :, 1:] - self.pre_emphasis * audio[:, :, :-1]), dim=-1)  # Pre Emphasize
         real_part, imag_part = self.stft_model(audio, 'constant')
         mel_features = torch.matmul(self.fbank, real_part * real_part + imag_part * imag_part).clamp(min=1e-5).log10()
