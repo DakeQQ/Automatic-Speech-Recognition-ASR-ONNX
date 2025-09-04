c601f7e16b8e9d69eb7e8d8f726007875c7fcd1d
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-09-04 22:12:13 +0800

    Update Export_FireRedASR_AED.py

--- FireRedASR/Beam_Search/Export_FireRedASR_AED.py
+++ FireRedASR/Beam_Search/Export_FireRedASR_AED.py
@@ -166,7 +166,7 @@ class FIRE_RED_ENCODER(torch.nn.Module):
         if self.pre_emphasis > 0:
             audio = torch.cat([audio[:, :, :1], audio[:, :, 1:] - self.pre_emphasis * audio[:, :, :-1]], dim=-1)
         real_part, imag_part = self.stft_model(audio, 'constant')
-        mel_features = torch.matmul(self.fbank, real_part * real_part + imag_part * imag_part).transpose(1, 2).clamp(min=1e-5).log()
+        mel_features = torch.matmul(self.fbank, real_part * real_part + imag_part * imag_part).transpose(1, 2).clamp(min=1e-7).log()
         mel_features = (mel_features - self.cmvn_means) * self.cmvn_vars
         enc_outputs = self.model.encoder(mel_features)
         for idx, decoder_layer in enumerate(self.model.decoder.layer_stack):
