dc0d8f8166b7148d378bb55200472d23b028f50a
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-09-04 22:14:10 +0800

    Update Export_Dolphin.py

--- Dolphin/Greedy_Search/Export_Dolphin.py
+++ Dolphin/Greedy_Search/Export_Dolphin.py
@@ -316,7 +316,7 @@ class DOLPHIN_ENCODER(torch.nn.Module):
         if self.pre_emphasis > 0:
             audio = torch.cat([audio[:, :, :1], audio[:, :, 1:] - self.pre_emphasis * audio[:, :, :-1]], dim=-1)
         real_part, imag_part = self.stft_model(audio, 'constant')
-        mel_features = torch.matmul(self.fbank, real_part * real_part + imag_part * imag_part).transpose(1, 2).clamp(min=1e-5).log()
+        mel_features = torch.matmul(self.fbank, real_part * real_part + imag_part * imag_part).transpose(1, 2).clamp(min=1e-7).log()
         mel_features = (mel_features - self.dolphin.normalize.mean) * self.inv_std
         embed = self.dolphin.encoder.embed.conv(mel_features.unsqueeze(0))
         embed_len = embed.shape[-2].unsqueeze(0)
