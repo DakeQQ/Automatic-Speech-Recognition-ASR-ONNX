cf2dc0f07302140a2c31d2ef1f209b7c1d6c5c3d
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-11-08 22:57:02 +0800

    Update Export_Paraformer.py

--- Paraformer_Chinese/Export_Paraformer.py
+++ Paraformer_Chinese/Export_Paraformer.py
@@ -76,12 +76,12 @@ class PARAFORMER(torch.nn.Module):
         self.indices_mel = indices.clamp(max=stft_signal_len + self.lfr_m_factor - 1)
         num_head = self.encoder.encoders._modules["0"].self_attn.h
         head_dim = self.encoder.encoders._modules["0"].self_attn.d_k
-        factor = float(head_dim ** (-0.25))
+        factor = float(head_dim ** (-0.5))
         total_encoders = list(self.encoder.encoders0) + list(self.encoder.encoders)
         cif_hidden_size_2 = cif_hidden_size + cif_hidden_size
         for encoder_layer in total_encoders:
-            encoder_layer.self_attn.linear_q_k_v.weight.data[:cif_hidden_size_2] *= factor
-            encoder_layer.self_attn.linear_q_k_v.bias.data[:cif_hidden_size_2] *= factor
+            encoder_layer.self_attn.linear_q_k_v.weight.data[:cif_hidden_size] *= factor
+            encoder_layer.self_attn.linear_q_k_v.bias.data[:cif_hidden_size] *= factor
             encoder_layer.self_attn.linear_q_w = encoder_layer.self_attn.linear_q_k_v.weight.data[:cif_hidden_size].view(num_head, head_dim, -1).transpose(1, 2).contiguous()
             encoder_layer.self_attn.linear_q_b = encoder_layer.self_attn.linear_q_k_v.bias.data[:cif_hidden_size].view(num_head, 1, head_dim).contiguous()
             encoder_layer.self_attn.linear_k_w = encoder_layer.self_attn.linear_q_k_v.weight.data[cif_hidden_size:cif_hidden_size_2].view(num_head, head_dim, -1).transpose(1, 2).contiguous()
@@ -93,7 +93,7 @@ class PARAFORMER(torch.nn.Module):
 
         num_head = self.decoder.decoders._modules["0"].src_attn.h
         head_dim = self.decoder.decoders._modules["0"].src_attn.d_k
-        factor = float(head_dim ** (-0.25))
+        factor = float(head_dim ** (-0.5))
         for decoder_layer in self.decoder.decoders:
             decoder_layer.src_attn.linear_q.weight.data *= factor
             decoder_layer.src_attn.linear_q.bias.data *= factor
@@ -101,8 +101,6 @@ class PARAFORMER(torch.nn.Module):
             decoder_layer.src_attn.linear_q_b = decoder_layer.src_attn.linear_q.bias.data.view(num_head, 1, head_dim).contiguous()
             decoder_layer.src_attn.linear_k_w = decoder_layer.src_attn.linear_k_v.weight.data[:cif_hidden_size].view(num_head, head_dim, -1).transpose(1, 2).contiguous()
             decoder_layer.src_attn.linear_k_b = decoder_layer.src_attn.linear_k_v.bias.data[:cif_hidden_size].view(num_head, 1, head_dim).contiguous()
-            decoder_layer.src_attn.linear_k_w *= factor
-            decoder_layer.src_attn.linear_k_b *= factor
             decoder_layer.src_attn.linear_v_w = decoder_layer.src_attn.linear_k_v.weight.data[cif_hidden_size:].view(num_head, head_dim, -1).transpose(1, 2).contiguous()
             decoder_layer.src_attn.linear_v_b = decoder_layer.src_attn.linear_k_v.bias.data[cif_hidden_size:].view(num_head, 1, head_dim).contiguous()
             decoder_layer.src_attn.linear_out_w = decoder_layer.src_attn.linear_out.weight.data.view(-1, num_head, head_dim).permute(1, 2, 0).contiguous()
