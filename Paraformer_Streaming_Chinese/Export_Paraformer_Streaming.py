bee0825e41dac5e3dae61bf2c73d670ac1b70e9e
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-11-09 09:28:29 +0800

    Update Export_Paraformer_Streaming.py

--- Paraformer_Streaming_Chinese/Export_Paraformer_Streaming.py
+++ Paraformer_Streaming_Chinese/Export_Paraformer_Streaming.py
@@ -81,12 +81,12 @@ class PARAFORMER_ENCODER(torch.nn.Module):
         self.position_encoding = self.encoder.embed.encode(positions, feature_size).half()
         num_head = self.encoder.encoders._modules["0"].self_attn.h
         head_dim = self.encoder.encoders._modules["0"].self_attn.d_k
-        factor = float(head_dim ** (-0.5))
+        factor = float(head_dim ** (-0.25))
         total_encoders = list(self.encoder.encoders0) + list(self.encoder.encoders)
         cif_hidden_size_2 = cif_hidden_size + cif_hidden_size
         for encoder_layer in total_encoders:
-            encoder_layer.self_attn.linear_q_k_v.weight.data[:cif_hidden_size] *= factor
-            encoder_layer.self_attn.linear_q_k_v.bias.data[:cif_hidden_size] *= factor
+            encoder_layer.self_attn.linear_q_k_v.weight.data[:cif_hidden_size_2] *= factor
+            encoder_layer.self_attn.linear_q_k_v.bias.data[:cif_hidden_size_2] *= factor
             encoder_layer.self_attn.linear_q_w = encoder_layer.self_attn.linear_q_k_v.weight.data[:cif_hidden_size].view(num_head, head_dim, -1).transpose(1, 2).contiguous()
             encoder_layer.self_attn.linear_q_b = encoder_layer.self_attn.linear_q_k_v.bias.data[:cif_hidden_size].view(num_head, 1, head_dim).contiguous()
             encoder_layer.self_attn.linear_k_w = encoder_layer.self_attn.linear_q_k_v.weight.data[cif_hidden_size:cif_hidden_size_2].view(num_head, head_dim, -1).transpose(1, 2).contiguous()
@@ -200,10 +200,12 @@ class PARAFORMER_DECODER(torch.nn.Module):
         self.save_values_de = [None] * cache_layer_num_de
         num_head = self.decoder.decoders._modules["0"].src_attn.h
         head_dim = self.decoder.decoders._modules["0"].src_attn.d_k
-        factor = float(head_dim ** (-0.5))
+        factor = float(head_dim ** (-0.25))
         for decoder_layer in self.decoder.decoders:
             decoder_layer.src_attn.linear_q.weight.data *= factor
             decoder_layer.src_attn.linear_q.bias.data *= factor
+            decoder_layer.src_attn.linear_k.weight.data *= factor
+            decoder_layer.src_attn.linear_k.bias.data *= factor
             decoder_layer.src_attn.linear_q_w = decoder_layer.src_attn.linear_q.weight.data.view(num_head, head_dim, -1).transpose(1, 2).contiguous()
             decoder_layer.src_attn.linear_q_b = decoder_layer.src_attn.linear_q.bias.data.view(num_head, 1, head_dim).contiguous()
             decoder_layer.src_attn.linear_k_w = decoder_layer.src_attn.linear_k_v.weight.data[:cif_hidden_size].view(num_head, head_dim, -1).transpose(1, 2).contiguous()
