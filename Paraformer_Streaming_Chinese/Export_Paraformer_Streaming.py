78aa6fff22abd55e1e3a281cd04ace3b284384f7
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-11-24 11:23:45 +0800

    Update Export_Paraformer_Streaming.py

--- Paraformer_Streaming_Chinese/Export_Paraformer_Streaming.py
+++ Paraformer_Streaming_Chinese/Export_Paraformer_Streaming.py
@@ -13,7 +13,7 @@ from STFT_Process import STFT_Process  # The custom STFT/ISTFT can be exported i
 model_path = "/home/DakeQQ/Downloads/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online"      # The Paraformer-Chinese-Online-Streaming download path.
 onnx_model_A = "/home/DakeQQ/Downloads/Paraformer_ONNX/Paraformer_Streaming_Encoder.onnx"                    # The exported onnx model path.
 onnx_model_B = "/home/DakeQQ/Downloads/Paraformer_ONNX/Paraformer_Streaming_Decoder.onnx"                    # The exported onnx model path.
-vocab_path = "/home/DakeQQ/Downloads/Paraformer_ONNX/Vocab_Paraformer.txt"                                   # The vocab list.
+vocab_path = "/home/DakeQQ/Downloads/Paraformer_ONNX/Vocab_Paraformer.txt"                                   # Save the vocab list.
 test_audio = "./zh.wav"                                                                                      # The test audio.
 
 
