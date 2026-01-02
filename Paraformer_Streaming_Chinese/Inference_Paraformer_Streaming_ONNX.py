ea0aff24b00326896f74eecfb3a4e5d99ee312d6
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2026-01-02 19:05:45 +0800

    Update Inference_Paraformer_Streaming_ONNX.py

--- Paraformer_Streaming_Chinese/Inference_Paraformer_Streaming_ONNX.py
+++ Paraformer_Streaming_Chinese/Inference_Paraformer_Streaming_ONNX.py
@@ -48,10 +48,12 @@ else:
     provider_options = None
 
 
-def normalize_to_int16(audio):
-    max_val = np.max(np.abs(audio))
-    scaling_factor = 32767.0 / max_val if max_val > 0 else 1.0
-    return (audio * float(scaling_factor)).astype(np.int16)
+def normalizer(_audio, target_value=8192.0):
+    _audio = _audio.astype(np.float32)
+    rms = np.sqrt(np.mean((_audio * _audio), dtype=np.float32), dtype=np.float32)
+    _audio *= (target_value / (rms + 1e-7))
+    np.clip(_audio, -32768.0, 32767.0, out=_audio)
+    return _audio.astype(np.int16)
 
 # ONNX Runtime settings
 session_opts = onnxruntime.SessionOptions()
@@ -113,7 +115,7 @@ for i in range(amount_of_outputs_B):
 
 # Load the input audio
 audio = np.array(AudioSegment.from_file(test_audio).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.float32)
-audio = normalize_to_int16(audio)
+audio = normalizer(audio)
 audio_len = len(audio)
 audio = audio.reshape(1, 1, -1)
 if isinstance(shape_value_in, str):
